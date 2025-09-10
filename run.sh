e41a124824ff1239f14565b1cfa3157b74f079ef
Kyle Andrews <codingwithkyle@gmail.com>
2025-09-10 17:23:58 -0400

    Improved script, some safeguarding, and added support for coloured text

--- run.sh
+++ run.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+PDF_FILE="$1"
+
+if [ -z "$PDF_FILE" ]; then
+  echo "Usage: ./run <pdf>"
+  exit 1
+fi
+
+if [[ ! "$PDF_FILE" =~ \.pdf$|\.PDF$ ]]; then
+  echo "Error: $PDF_FILE is not a PDF file."
+  exit 1
+fi
+
+if [ ! -f "$PDF_FILE" ]; then
+  echo "Error: File '$PDF_FILE' not found."
+  exit 1
+fi
+
 # KEEP
 #gs -dSAFER -dBATCH -dNOPAUSE \
    #-sDEVICE=png16m -r300 \
@@ -18,7 +35,7 @@ gs -dSAFER -dBATCH -dNOPAUSE \
   -dUseCropBox -dFitPage \
   -g480x800 \
   -dFILTERTEXT \
-  -sOutputFile=./temp/img_480x800.png ./base/sign.pdf
+  -sOutputFile=./temp/img_480x800.png "$PDF_FILE"
 
 # text only
 echo "Render text only..."
@@ -29,7 +46,7 @@ gs -dSAFER -dBATCH -dNOPAUSE \
   -dFILTERIMAGE -dFILTERVECTOR \
   -dTextAlphaBits=1 -dGraphicsAlphaBits=1 \
   -dBackgroundColor=16#00000000 \
-  -sOutputFile=./temp/text_480x800.png ./base/sign.pdf
+  -sOutputFile=./temp/text_480x800.png "$PDF_FILE"
 
 echo "Dithering and quantizing background..."
 convert ./temp/img_480x800.png \
@@ -37,7 +54,28 @@ convert ./temp/img_480x800.png \
    -ordered-dither o2x2 -remap ./base/LUT6.png \
    ./temp/bg_dither_6c.png
 
+echo "Extract alpha from text…"
+convert ./temp/text_480x800.png \
+  -alpha extract -colorspace Gray \
+  +profile icc +profile icm -strip \
+  -define png:color-type=0 \
+  ./temp/text_alpha.png
+
+echo "Quantize text colors without alpha…"
+convert ./temp/text_480x800.png \
+  -alpha off \
+  -dither None \
+  -remap ./base/LUT6.png \
+  -colorspace sRGB -type TrueColor -define png:color-type=2 \
+  +profile icc +profile icm -strip \
+  ./temp/text_rgb_6c.png
+
+echo "Restore alpha…"
+convert ./temp/text_rgb_6c.png ./temp/text_alpha.png \
+  -compose CopyOpacity -composite \
+  PNG8:./temp/text_6c.png
+
 echo "Merging into final image..."
-convert ./temp/bg_dither_6c.png ./temp/text_480x800.png -compose over -composite output.png
+convert ./temp/bg_dither_6c.png ./temp/text_6c.png -compose over -composite output.png
 echo "Done!"
 
